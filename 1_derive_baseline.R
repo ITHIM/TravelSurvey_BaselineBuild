728bc5b804604d117a07bd050a1a48bb87b9e112
audev <au232@medschl.cam.ac.uk>
2017-02-17 17:28:13 +0000

    Update bl build

--- 1_derive_baseline.R
+++ 1_derive_baseline.R
@@ -20,7 +20,7 @@ household2014  <- household2014[ ,c('HouseholdID', 'HHoldGOR_B02ID')]
 
 
 # filter days/stages
-day2014 <-  day2014[ ,]
+#day2014 <-  day2014[ ,]
 stage2014 <- stage2014[ ,c('SurveyYear', 'StageID', 'TripID', 'DayID', 
                            'IndividualID', 'HouseholdID', 'PSUID', 'VehicleID', 
                            'IndTicketID', 'PersNo', 'TravDay', 'JourSeq', 'StageMode_B03ID',
@@ -64,23 +64,21 @@ bl2014$agesex <- paste0(bl2014$Age,bl2014$Sex)
 
 ################ CYCLABLE TRIPS  ##################
 
-#create cyclable trips  
-str_sql <- 'SELECT T1.SurveyYear, T1.TripID, T1.IndividualID, T2.TravDay, 
-T1.W5, T1.W5xHH, T1.SeriesCall_B01ID, T1.ShortWalkTrip_B01ID,
-T1.NumStages, T1.MainMode_B03ID, T1.TripTotalTime, T1.TripTravTime,  
-T1.TripDisIncSW, T1.TripDisExSW, T1.JJXSC, T1.JOTXSC, T1.JTTXSC, T1.JD, 
-0 AS Cycl_Impossible, T3.Age_B01ID, 
-T3.Sex_B01ID, T3.NSSec_B03ID, T3.CarAccess_B01ID
-
-FROM (trip2014 as T1 INNER JOIN day2014 as T2 ON T1.DayID = T2.DayID)
-INNER JOIN ind2014 as T3 ON T1.IndividualID = T3.IndividualID
-
-WHERE (((T3.Age_B01ID)>=8)) '
+#generate cyclable trips  
+str_sql <- 'SELECT T1.SurveyYear, T1.TripID, T1.IndividualID, 
+            
+            T1.W5, T1.W5xHH, T1.SeriesCall_B01ID, T1.ShortWalkTrip_B01ID,
+            T1.NumStages, T1.MainMode_B03ID, T1.TripTotalTime, T1.TripTravTime,  
+            T1.TripDisIncSW, T1.TripDisExSW, T1.JJXSC, T1.JOTXSC, T1.JTTXSC, T1.JD, 
+            0 AS Cycl_Impossible,
+            T3.Age_B01ID, T3.Sex_B01ID, T3.NSSec_B03ID, T3.CarAccess_B01ID
+            
+            FROM (trip2014 as T1 INNER JOIN ind2014 as T3 ON T1.IndividualID = T3.IndividualID '
 
 tripscyclable <-sqldf(x=str_sql)
 
 
-############### WALKABLE TRIPS  (= trips w. walked stages)
+############### WALKED/CYCLED TRIPS  (= trips w. P.A. stages)
 
 str_sql <- 'SELECT T1.TripID, T2.StageID, T4.TravDay, T2.IndividualID, T2.StageSeq, T2.StageDistance, 
 T2.StageTime, T2.StageTime_B01ID, T2.StageMode_B03ID, T2.StageMode_B04ID, T2.StageMode_B11ID, 
@@ -91,16 +89,14 @@ FROM ((trip2014 AS T1 INNER JOIN stage2014 AS T2 ON T1.TripID = T2.TripID)
 INNER JOIN ind2014 AS T3 ON T2.IndividualID = T3.IndividualID) 
 INNER JOIN day2014 AS T4 ON T2.DayID = T4.DayID
 
-WHERE (((T2.StageMode_B04ID)=1) AND ((T3.Age_B01ID)>=8))
+WHERE (  T2.StageMode_B04ID IN (1,2)   )
 
 ORDER BY T1.TripID '
 
-# columns not in dataset:    T2.SD_woW5, T2.SSXSC_woW5, T2.STTXSC_woW5, 
-
-tripswalkstages <- sqldf(x=str_sql)
+tripswc <- sqldf(x=str_sql)
 
 
-#####################  COMBINE: CYCLABLE trips + WALKABLE stages:
+#####################  COMBINE: bl trips + W/C stages METs times:
 
 str_sql <- 'SELECT T1.SurveyYear, T1.TripID, T1.NumStages, 
 T1.IndividualID, T1.TravDay, T1.SeriesCall_B01ID, 
@@ -110,15 +106,15 @@ T2.StageMode_B04ID, T2.StageDistance, T2.StageTime,
 T2.W5, T2.W5xHH, T2.SSXSC, T2.STTXSC, 
 T2.SD
 
-FROM tripscyclable AS T1 LEFT JOIN tripswalkstages AS T2 ON T1.TripID = T2.TripID
+FROM tripscyclable AS T1 LEFT JOIN tripswc AS T2 ON T1.TripID = T2.TripID
 
 ORDER BY T1.SurveyYear, T1.NumStages DESC , T2.StageID '
 
-#NON-EXISTENT COLUMNS:  , T2.SD_woW5, T2.SSXSC_woW5, T2.STTXSC_woW5
+
 wc <- sqldf(x=str_sql)    #cyclable trips w. walked stages added
 
 
-###### calculate  TIMES/DISTANCES (for walking stages)
+###### calculate  TIMES/DISTANCES (for METs stages) for main parameters
 str_sql <- 'SELECT wc.TripID, Sum(wc.StageDistance) AS SumofWStageDistance, 
 Sum(wc.StageTime) AS SumOfWStageTime, Sum(wc.STTXSC) AS SumOfSTTXSC, 
 Sum(wc.SD) AS SumOfSD
